6ce8410f3250e319195912e20bcaa29cbd295a96
Akash Kumar Prajapat <rkpsx7@gmail.com>
2021-11-19 19:34:27 +0530

    1st commit

---
+++ app/src/main/java/com/masai/lifesaver/ui/activity/InstantActionActivity.kt
@@ -0,0 +1,15 @@
+package com.masai.lifesaver.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.masai.lifesaver.R
+
+class InstantActionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instant_action)
+
+
+
+    }
+}
