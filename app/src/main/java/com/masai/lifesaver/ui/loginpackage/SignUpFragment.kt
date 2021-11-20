726b4275ac7e317780e07f2f9e1de0c22e3f3109
AmolPardeshi99 <pardeshiamol772@gmail.com>
2021-11-21 02:16:10 +0530

    Added signin sign up

---
+++ app/src/main/java/com/masai/lifesaver/ui/loginpackage/SignUpFragment.kt
@@ -0,0 +1,21 @@
+package com.masai.lifesaver.ui.loginpackage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.masai.lifesaver.R
+
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+
+    lateinit var navController: NavController
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+
+    }
+
+
+}
