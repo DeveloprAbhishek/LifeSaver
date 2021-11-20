490a91b91b17aae9b0271d84e74c9ef79b5fbad0
Akash Kumar Prajapat <rkpsx7@gmail.com>
2021-11-20 14:06:37 +0530

    Layouts

--- app/src/main/java/com/masai/lifesaver/recyclerview/RecordAdapter.kt
+++ app/src/main/java/com/masai/lifesaver/recyclerview/RecordAdapter.kt
@@ -6,27 +6,35 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.masai.lifesaver.R
 import com.masai.lifesaver.models.BookingRecordModel
+import kotlinx.android.synthetic.main.booking_rec_item_layout.view.*
 
-class RecordAdapter(recordList: ArrayList<BookingRecordModel>) :
+class RecordAdapter(private val recordList: ArrayList<BookingRecordModel>) :
     RecyclerView.Adapter<RecordAdapter.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.booking_rec_item_layout,parent,false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.booking_rec_item_layout, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = recordList[position]
+        holder.setData(item)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return recordList.size
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-
-        //fun setData(item:)
+    class ViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun setData(item: BookingRecordModel) {
+            itemView.apply {
+                tv_record_date.text = item.date
+                tv_record_time.text = item.time
+                tv_address_from.text = item.from
+                tv_address_to.text = item.to
+            }
+        }
     }
 }
--- app/src/main/java/com/masai/lifesaver/ui/activity/userhome/PreviBookingsFragment.kt
+++ app/src/main/java/com/masai/lifesaver/ui/activity/userhome/PreviBookingsFragment.kt
@@ -1,12 +1,62 @@
 package com.masai.lifesaver.ui.activity.userhome
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.masai.lifesaver.R
+import com.masai.lifesaver.models.BookingRecordModel
+import com.masai.lifesaver.recyclerview.RecordAdapter
+import kotlinx.android.synthetic.main.fragment_previ_bookings.*
 
 class PreviBookingsFragment : Fragment(R.layout.fragment_previ_bookings) {
+    private var recordList = ArrayList<BookingRecordModel>()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val item = BookingRecordModel(
+            "Abhishek Colony",
+            "SMS Hospital",
+            "12-12-12",
+            "9AM",
+            "Anol",
+            7845121215
+        )
+        val ite2 = BookingRecordModel(
+            "Abhishek Colony",
+            "SMS Hospital",
+            "12-12-12",
+            "9AM",
+            "Anol",
+            7845121215
+        )
+        val item3 = BookingRecordModel(
+            "Abhishek Colony",
+            "SMS Hospital",
+            "12-12-12",
+            "9AM",
+            "Anol",
+            7845121215
+        )
+        val ite4 = BookingRecordModel(
+            "Abhishek Colony",
+            "SMS Hospital",
+            "12-12-12",
+            "9AM",
+            "Anol",
+            7845121215
+        )
+        recordList.add(item)
+        recordList.add(ite2)
+        recordList.add(ite4)
+        recordList.add(item3)
+        setRecyclerView()
+
+    }
+
+    private fun setRecyclerView() {
+        recyclerViewRecords.adapter = RecordAdapter(recordList)
+        recyclerViewRecords.layoutManager = LinearLayoutManager(requireActivity())
+    }
 }
