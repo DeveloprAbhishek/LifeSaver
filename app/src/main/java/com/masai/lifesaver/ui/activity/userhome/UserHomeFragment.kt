97c56672087c4fcfa508485d3860b7fff1547189
AmolPardeshi99 <pardeshiamol772@gmail.com>
2021-11-21 13:35:58 +0530

    bug fix4

--- app/src/main/java/com/masai/lifesaver/ui/activity/userhome/UserHomeFragment.kt
+++ app/src/main/java/com/masai/lifesaver/ui/activity/userhome/UserHomeFragment.kt
@@ -1,9 +1,23 @@
 package com.masai.lifesaver.ui.activity.userhome
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.masai.lifesaver.R
+import com.masai.lifesaver.ui.activity.EmergencyActivity
+import kotlinx.android.synthetic.main.activity_booking_records.*
 
 class UserHomeFragment : Fragment(R.layout.activity_booking_records) {
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        btnEmergencyfromHome.setOnClickListener {
+            startActivity(Intent(requireActivity(),EmergencyActivity::class.java))
+        }
+    }
 
 }
