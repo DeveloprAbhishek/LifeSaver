f0e54dabc7d8136c27d62751324fb49230e82633
Akash Kumar Prajapat <rkpsx7@gmail.com>
2021-11-21 14:55:16 +0530

    Calender

--- app/src/main/java/com/masai/lifesaver/ui/activity/userhome/UserHomeFragment.kt
+++ app/src/main/java/com/masai/lifesaver/ui/activity/userhome/UserHomeFragment.kt
@@ -1,12 +1,17 @@
 package com.masai.lifesaver.ui.activity.userhome
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.masai.lifesaver.R
 import com.masai.lifesaver.ui.activity.EmergencyActivity
 import kotlinx.android.synthetic.main.activity_booking_records.*
+
 
 class UserHomeFragment : Fragment(R.layout.activity_booking_records) {
 
@@ -14,9 +19,32 @@ class UserHomeFragment : Fragment(R.layout.activity_booking_records) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btn_call.setOnClickListener {
+            val callIntent = Intent(Intent.ACTION_CALL)
+            callIntent.data = Uri.parse("tel:108")
+            if (ActivityCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.CALL_PHONE
+                    )
+                ) {
+                } else {
+                    ActivityCompat.requestPermissions(
+                        requireActivity(), arrayOf(Manifest.permission.CALL_PHONE),
+                        7
+                    )
+                }
+            }
+            startActivity(callIntent)
+        }
+
 
         btnEmergencyfromHome.setOnClickListener {
-            startActivity(Intent(requireActivity(),EmergencyActivity::class.java))
+            startActivity(Intent(requireActivity(), EmergencyActivity::class.java))
         }
     }
 
--- app/src/main/java/com/masai/lifesaver/ui/loginpackage/InstantActionActivity.kt
+++ app/src/main/java/com/masai/lifesaver/ui/loginpackage/InstantActionActivity.kt
@@ -1,13 +1,35 @@
 package com.masai.lifesaver.ui.loginpackage
 
-import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.masai.lifesaver.R
-import com.masai.lifesaver.ui.activity.EmergencyActivity
-import kotlinx.android.synthetic.main.activity_instant_action.*
 
 class InstantActionActivity : AppCompatActivity() {
+
+    override fun onStart() {
+        super.onStart()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CALL_PHONE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.CALL_PHONE
+                )
+            ) {
+            } else {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(Manifest.permission.CALL_PHONE),
+                    7
+                )
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instant_2)
