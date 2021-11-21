6a28943e812c256b3ad1bd8ee66c7292b2e00e28
Akash Kumar Prajapat <rkpsx7@gmail.com>
2021-11-21 09:57:51 +0530

    Calender

---
+++ app/src/main/java/com/masai/lifesaver/ui/activity/BookingDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.masai.lifesaver.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.masai.lifesaver.R
+
+class BookingDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_booking_details)
+    }
+}
